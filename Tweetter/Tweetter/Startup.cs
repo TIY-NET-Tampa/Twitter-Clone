c9da970583635e0e94c72df14429a81708f8d1dc
Mark Dewey <mark.t.dewey@gmail.com>
2017-05-01 13:40:33 -0400

    added base code for auth and custom error

---
+++ Tweetter/Tweetter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tweetter.Startup))]
+namespace Tweetter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
